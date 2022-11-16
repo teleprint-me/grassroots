2bf6eec57176e923ca47eb3bae39d8c51e56cdd9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-11-16 17:34:22 -0500

    Add components
    - Add a component skeleton file
    - Add a sample component

---
+++ components/time-format.mjs
@@ -0,0 +1,45 @@
+class TimeFormat extends HTMLElement {
+    constructor () {
+        this.rendered = false;
+    }
+
+    render() {
+        let date = new Date(this.getAttribute('datetime') || Date.now());
+
+        this.innerHTML = new Intl.DateTimeFormat('default', {
+            year: this.getAttribute('year') || undefined,
+            month: this.getAttribute('month') || undefined,
+            day: this.getAttribute('day') || undefined,
+            hour: this.getAttribute('hour') || undefined,
+            minute: this.getAttribute('minute') || undefined,
+            second: this.getAttribute('second') || undefined,
+            timeZoneName: this.getAttribute('time-zone-name') || undefined
+        }).format(date);
+
+        setInterval(() => this.setAttribute('datetime', new Date()), 1000);
+    }
+
+    connectedCallback() {
+        if (!this.rendered) {
+            this.render();
+            this.rendered = true;
+        }
+    }
+
+    static get observedAttributes() {
+        return [
+            'datetime',
+            'year',
+            'month',
+            'day',
+            'hour',
+            'minute',
+            'second',
+            'time-zone-name'
+        ];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render();
+    }
+}
