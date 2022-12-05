efa207e24d8254608ed7a7cdfde71505c7ffa521
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2022-12-05 13:34:49 -0500

    Refactor requests
    - Sperate XMLHttpRequests from fetch requests
    - Leverage the built-in features of XMLHttpRequests on their own merit
    - Wrap XMLHttpRequests and fetch to simplify calling requests
    - Use promises for XMLHttpRequests
    - Use async and await for fetch

---
+++ modules/async-request.mjs
@@ -0,0 +1,22 @@
+export class AsyncRequest {
+    async text(url) {
+        return await fetch(url).then((response) => response.text());
+    }
+
+    async json(url) {
+        return await fetch(url).then((response) => response.json());
+    }
+
+    async blob(url) {
+        return await fetch(url).then((response) => response.blob());
+    }
+
+    async template(url) {
+        const html = await this.text(url);
+        const parser = new DOMParser();
+        const template = document.createElement('template');
+        const document = parser.parseFromString(html, 'text/html');
+        template.appendChild(document);
+        return template.content;
+    }
+}
